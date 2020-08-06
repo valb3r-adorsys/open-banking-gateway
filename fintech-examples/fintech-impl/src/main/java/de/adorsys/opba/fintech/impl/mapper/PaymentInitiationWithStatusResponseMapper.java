01ba79fe6207929846734dd7a78f1f39cb3a9271
Maxim Grischenko <grischenko.maxim@gmail.com>
2020-08-06 09:59:53 +0200

    #893 payment status with sca

--- fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/mapper/PaymentInitiationWithStatusResponseMapper.java
+++ fintech-examples/fintech-impl/src/main/java/de/adorsys/opba/fintech/impl/mapper/PaymentInitiationWithStatusResponseMapper.java
@@ -1,9 +1,10 @@
 package de.adorsys.opba.fintech.impl.mapper;
 
 import de.adorsys.opba.fintech.api.model.generated.PaymentInitiationWithStatusResponse;
+import de.adorsys.opba.tpp.pis.api.model.generated.PaymentInformationResponse;
 import org.mapstruct.Mapper;
 
 @Mapper(implementationPackage = "de.adorsys.opba.fintech.impl.mapper.generated")
 public abstract class PaymentInitiationWithStatusResponseMapper {
-    public abstract PaymentInitiationWithStatusResponse mapFromTppToFintech(de.adorsys.opba.tpp.pis.api.model.generated.PaymentInitiationWithStatusResponse response);
+    public abstract PaymentInitiationWithStatusResponse mapFromTppToFintech(PaymentInformationResponse response);
 }
